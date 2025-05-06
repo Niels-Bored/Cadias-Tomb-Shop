3d03c98be9f214f4499a6462dd6fa731173c3c61
Abel Soto Valdez <abelsotovaldez@MacBook-Air-de-Abel.local>
2025-05-05 18:30:16 -0600

    feat(tests): test add product to cart

--- core/tests_base/tests_views.py
+++ core/tests_base/tests_views.py
@@ -9,6 +9,121 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
 
+class TestSeleniumBase(LiveServerTestCase):
+    """Base class to test frontend with selenium (login and setup)"""
+
+    def setUp(self, endpoint="/login/", auto_login: bool = True):
+        """Load data, setup and login in each test
+        
+        Args:
+            endpoint (str): Endpoint to open in the browser
+            auto_login (bool): If True, login automatically in the browser
+        """
+
+        # Create admin user
+        self.user_username, self.user_pass, self.user = self.create_user()
+
+        # Setup selenium
+        self.endpoint = endpoint
+        self.__setup_selenium__()
+        if auto_login:
+            self.login()
+
+    def tearDown(self):
+        """Close selenium"""
+        try:
+            self.driver.quit()
+        except Exception:
+            pass
+    
+    def create_user(self):
+        username = "test_user@gmail.com"
+        password = "test_password"
+        user = User.objects.create_user(
+            username,
+            password=password,
+            email=username,
+            is_staff=False,
+        )
+
+        return username, password, user
+
+    def __setup_selenium__(self):
+        """Setup and open selenium browser"""
+        chrome_options = Options()
+
+        # Run in headless mode if enabled
+        if settings.TEST_HEADLESS:
+            chrome_options.add_argument("--headless=new")
+            chrome_options.add_argument("--disable-gpu")
+
+        # Allow clipboard access
+        prefs = {"profile.default_content_setting_values.clipboard": 1}
+        chrome_options.add_experimental_option("prefs", prefs)
+
+        # Disable Chrome automation infobars and password save popups
+        chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        chrome_options.add_experimental_option("useAutomationExtension", False)
+        chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+
+        self.driver = webdriver.Chrome(options=chrome_options)
+        self.driver.implicitly_wait(5)
+
+    def login(self):
+
+        # Load login page and get fields
+        self.driver.get(f"{self.live_server_url}/login/")
+        sleep(2)
+        selectors_login = {
+            "username": "#username",
+            "password": "#password",
+            "submit": "button[type='submit']",
+        }
+        fields_login = self.get_selenium_elems(selectors_login)
+
+        fields_login["username"].send_keys(self.user_username)
+        fields_login["password"].send_keys(self.user_pass)
+        fields_login["submit"].click()
+
+        # Wait after login
+        sleep(3)
+
+        # Open page
+        self.driver.get(f"{self.live_server_url}{self.endpoint}")
+        sleep(2)
+
+    def set_page(self, endpoint):
+        """Set page"""
+        self.driver.get(f"{self.live_server_url}{endpoint}")
+        sleep(2)
+        
+    def get_selenium_elem(self, selector: str) -> WebElement:
+        """Get selenium element from selector
+
+        Args:
+            selector (str): css selector to find
+
+        Returns:
+            WebElement: selenium element
+        """
+        try:
+            return self.driver.find_element(By.CSS_SELECTOR, selector)
+        except Exception:
+            return None
+
+    def get_selenium_elems(self, selectors: dict) -> dict[str, WebElement]:
+        """Get selenium elements from selectors
+
+        Args:
+            selectors (dict): css selectors to find: name, value
+
+        Returns:
+            dict[str, WebElement]: selenium elements: name, value
+        """
+        fields = {}
+        for key, value in selectors.items():
+            fields[key] = self.get_selenium_elem(value)
+        return fields
 
 class TestViewBase(LiveServerTestCase):
     """Base class to test users (login and setup)"""
--- shop/tests/tests_views.py
+++ shop/tests/tests_views.py
@@ -1,31 +1,36 @@
-from django.contrib.auth.models import User
-from django.test import TestCase
-from django.urls import reverse
+from time import sleep
 
-from core.tests_base.tests_views import TestViewBase
+from django.contrib.auth.models import User
+from django.test import TestCase, LiveServerTestCase
+from django.urls import reverse
+from django.conf import settings
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+
+from core.tests_base.tests_views import TestViewBase, TestSeleniumBase
+from shop import models
 
 
 class LoginViewTestCase(TestViewBase):
-    """Class to test user login
-    """
+    """Class to test user login"""
 
     def setUp(self):
-        """Define url and CSS selectors
-        """
+        """Define url and CSS selectors"""
         super().setUp("/login")
         self.selectors = {"logout_btn": ".logout-svg", "error_message": ".login-error"}
 
     def test_created_user(self):
-        """Try to login user
-        """
+        """Try to login user"""
         self.login()
         elems = self.get_selenium_elems(self.selectors)
 
         self.assertIsNotNone(elems["logout_btn"])
 
     def test_not_created_user(self):
-        """Try to login not created user
-        """
+        """Try to login not created user"""
         # Delete created user
         User.objects.all().delete()
 
@@ -36,8 +41,7 @@ class LoginViewTestCase(TestViewBase):
         self.assertIsNone(elems["logout_btn"])
 
     def test_incorrect_credentials(self):
-        """Try to login with incorrect password
-        """
+        """Try to login with incorrect password"""
         # Login with wrong username
         self.login("juanito")
 
@@ -51,22 +55,20 @@ class LoginViewTestCase(TestViewBase):
 
 
 class SignUpViewTestCase(TestCase):
-    """Class to test user signup
-    """
+    """Class to test user signup"""
+
     def setUp(self):
         pass
 
     def __create_user__(self):
-        """Create user for created user test case scenarios
-        """
+        """Create user for created user test case scenarios"""
         User.objects.create_user(
             username="juanito", email="juan@gmail.com", password="12345678"
         )
 
     def test_new_user(self):
-        """Try to create new user
-        """
-        url = reverse("signup") 
+        """Try to create new user"""
+        url = reverse("signup")
 
         data = {
             "first_name": "Juan",
@@ -87,8 +89,7 @@ class SignUpViewTestCase(TestCase):
         self.assertTrue(User.objects.filter(username="juanito").exists())
 
     def test_invalid_username(self):
-        """Try to create user when username is already been used 
-        """
+        """Try to create user when username is already been used"""
         self.__create_user__()
 
         url = reverse("signup")  # Asegúrate que este nombre coincida con tus urls
@@ -111,11 +112,10 @@ class SignUpViewTestCase(TestCase):
         self.assertContains(response, 'class="signup-error"')
 
         # Verifica que el usuario no fue creado
-        self.assertEqual(User.objects.all().count(),1)
-    
+        self.assertEqual(User.objects.all().count(), 1)
+
     def test_invalid_email(self):
-        """Try to create user when email is already been used 
-        """
+        """Try to create user when email is already been used"""
         self.__create_user__()
 
         url = reverse("signup")  # Asegúrate que este nombre coincida con tus urls
@@ -138,42 +138,104 @@ class SignUpViewTestCase(TestCase):
         self.assertContains(response, 'class="signup-error"')
 
         # Verifica que el usuario no fue creado
-        self.assertEqual(User.objects.all().count(),1)
+        self.assertEqual(User.objects.all().count(), 1)
 
 
 class LogOutViewTestCase(TestCase):
-    """Class to test user logout
-    """
+    """Class to test user logout"""
+
     def setUp(self):
         pass
 
     def test_session_closed():
         pass
 
-class CartViewTestCase(TestCase):
-    """Class to test user cart actions
-    """
+
+class CartViewTestCase(TestSeleniumBase):
+    """Class to test user cart actions"""
+
     def setUp(self):
+        super().setUp("/cart/")
+
+        # Create data
+        self.product1 = models.Producto.objects.create(
+            nombre="Stompa",
+            imagen="https://www.warhammer.com/app/resources/catalog/product/920x950/99120103021_StompaNEW01.jpg?fm=webp&w=920&h=948",
+            precio=3000,
+            marca="Games Workshop",
+            stock=3,
+        )
+
+        # Test variables
+        self.selectors = {
+            "add_btn": "a.product-item span"
+        }
+
+    def tearDown(self):
+        """Close selenium"""
+        try:
+            self.driver.quit()
+        except Exception:
+            pass
+
+    """ def test_try_buy_without_login(self):
+        input("hola") """
+    
+    def add_product(self):
+        """
+        Add product to cart
+        """
+        selectors = {
+            "alert_title": "#swal2-title"
+        }
+
+        # load product page
+        self.set_page("/shop/1")
+        
+        # Click to add product with js
+        self.driver.execute_script(f"""document.querySelector("{self.selectors['add_btn']}").click()""")
+        sleep(3)
+        
+        # Validate alert text
+        elems = self.get_selenium_elems(selectors)
+        self.assertIn("Producto añadido", elems["alert_title"].text)
+
+    def test_add_product_logged(self):
+        """
+        Try to add a product on cart logged.
+        Expected: update localstorage
+        """
+
+        selectors = {
+            "cart-product": "td.product-name"
+        }
+
+        # add product to cart
+        self.add_product()
+
+        # load cart page
+        self.set_page("/cart/")
+
+        elems = self.get_selenium_elems(selectors)
+        self.assertIn("Stompa", elems["cart-product"].text)
+
+    """
+    def test_remove_product(self):
         pass
 
-    def test_add_product():
+    def test_stock_not_exceeded(self):
         pass
 
-    def test_remove_product():
+    def test_stock_exceeded(self):
         pass
 
-    def test_stock_not_exceeded():
-        pass
+    def test_zero_amount_in_stock(self):
+        pass """
 
-    def test_stock_exceeded():
-        pass
-
-    def test_not_zero_amount():
-        pass
 
 class ShopViewTestCase(TestCase):
-    """Class to test user actions on shop
-    """
+    """Class to test user actions on shop"""
+
     def setUp(self):
         pass
 
@@ -182,10 +244,11 @@ class ShopViewTestCase(TestCase):
 
     def test_stock_exceeded():
         pass
+
 
 class HomeViewTestCase(TestCase):
-    """Class to test user actions on home
-    """
+    """Class to test user actions on home"""
+
     def setUp(self):
         pass
 
@@ -195,9 +258,9 @@ class HomeViewTestCase(TestCase):
     def test_stock_exceeded():
         pass
 
+
 class BlogViewTestCase(TestCase):
-    """Class to test user actions on blog
-    """
+    """Class to test user actions on blog"""
+
     def setUp(self):
         pass
-
