65f329792faa83e7552e3fac6014a15ec61487a1
unknown <abelsotovaldez@gmail.com>
2026-06-30 18:43:58 -0600

    feat(shop/admin.py): add fee model to admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,24 +1,44 @@
 from django.contrib import admin
 from shop import models
 
+
 # Register your models here.
 @admin.register(models.Producto)
 class ProductosAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'precio', 'marca', 'stock')
-    search_fields = ('nombre','marca')
-    list_filter = ('marca',)
+    list_display = ("nombre", "precio", "marca", "stock")
+    search_fields = ("nombre", "marca")
+    list_filter = ("marca",)
+
 
 @admin.register(models.Venta)
 class VentasAdmin(admin.ModelAdmin):
-    list_display = ('id', 'status', 'usuario', 'direccion', 'estado', 'codigo_postal', 'correo', 'telefono', 'fecha_venta', 'total')
-    search_fields = ('usuario__username',)
+    list_display = (
+        "id",
+        "status",
+        "usuario",
+        "direccion",
+        "estado",
+        "codigo_postal",
+        "correo",
+        "telefono",
+        "fecha_venta",
+        "total",
+    )
+    search_fields = ("usuario__username",)
+
 
 @admin.register(models.Blog)
 class BlogsAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'descripcion', 'autor', 'fecha_publicacion')
-    search_fields = ('titulo','autor')
-    filter_horizontal = ('tags',)
-    
+    list_display = ("titulo", "descripcion", "autor", "fecha_publicacion")
+    search_fields = ("titulo", "autor")
+    filter_horizontal = ("tags",)
+
+
 @admin.register(models.Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('nombre',)
+    list_display = ("nombre",)
+
+
+@admin.register(models.CuotaEnvio)
+class CuotaEnvioAdmin(admin.ModelAdmin):
+    list_display = ("precio",)
